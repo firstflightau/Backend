e4315464209fc8c38c94b0bf2d9dd809ad702873
Qamar Ali <qamarali205@gmail.com>
2025-09-14 15:40:33 +0530

    update with booking history schema

--- src/models/flightBooking.model.js
+++ src/models/flightBooking.model.js
@@ -1,12 +1,73 @@
 const mongoose = require("mongoose");
 
+const passengerDetailsSchema = new mongoose.Schema({
+  title: String,
+  firstName: String,
+  lastName: String,
+  dob: String,
+  gender: String,
+  passportNumber: String,
+  passportExpire: String,
+  passportIssueCountry: String,
+  paxType: String,
+  amount: Number,
+  ticketNumber: String,
+}, { _id: false });
+
+
+const airlineDetailsSchema = new mongoose.Schema({
+  departure: {
+    location: String,
+    date: String,
+    time: String,
+    terminal: String
+  },
+  arrival: {
+    location: String,
+    date: String,
+    time: String,
+    terminal: String
+  },
+  duration: String,
+  carrier: String,
+  number: String
+}, { _id: false });
+
 const flightBookingSchema = new mongoose.Schema(
   {
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
-    pnr:String,
+    pnr:{
+      type: String,
+      default: 'NA',
+      required: true,
+    },
+    totalAmount: Number,
+    markup: Number,
+    email: String,
+    onward:{
+      origin: String,
+      desination: String,
+      airlineDetails: [airlineDetailsSchema],
+    },
+    return:{
+      origin: String,
+      desination: String,
+      airlineDetails: [airlineDetailsSchema],
+    },
+    passengerDetails: [passengerDetailsSchema],
+    status: {
+      type: String,
+      enum: ['pending', 'failed', 'success'],
+      default: 'pending',
+    },
+    paymentStatus: {
+       type: String,
+       enum: ['pending', 'failed', 'success'],
+       default: 'pending',
+    }
   },
   {
     timestamps: true,
